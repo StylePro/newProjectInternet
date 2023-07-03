3fb25afe7fe7145618286dcd1dcda6acb3b1660a
style-pro <sergeev.gold@gmail.com>
2023-07-03 18:29:32 +0300

    download info server and USE !!!

--- src/components/API/PostService.jsx
+++ src/components/API/PostService.jsx
@@ -3,6 +3,6 @@ import axios from "axios";
 export default class PostService {
     static async getAll () {
             const responce = await axios.get('https://jsonplaceholder.typicode.com/posts')
-            return responce.data
+            return responce
     }
 }
